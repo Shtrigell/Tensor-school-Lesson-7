73dc84a78a69492b7495dbdda90e2e9c9d098c4e
Shtrigell <119353277+Shtrigell@users.noreply.github.com>
2022-12-21 23:11:15 +0300

    Add files via upload

---
+++ Tensor_chool_Lesson_7_2.py
@@ -0,0 +1,5 @@
+import numpy as np
+import random
+a = np.random.rand(3,3)
+print("Исходный массив:\n",a,"\n Транспонированный массив:\n",np.transpose(a))
+
---
+++ Tensor_school_Lesson_7_1.py
@@ -0,0 +1,3 @@
+import os
+print("OS: ", os.name,"\nИмя пользователя: ", os.getlogin(),"\nСписок файлов и директорий в папке: ",os.listdir() )
+
